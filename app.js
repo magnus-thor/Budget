2407863935f3dbdd743c4725055791213d5b3ae5
magnus-thor <magnus.thor.ar@gmail.com>
2017-12-05 12:11:25 +0100

    created model, view and controller

---
+++ app.js
@@ -0,0 +1,24 @@
+// Model
+var budgetController = (function() {
+
+    // some code
+
+})();
+
+// View
+var UIController = (function() {
+
+    // some code
+
+})();
+
+
+// Controller
+var controller = (function(budgetCtrl, UICtrl) {
+
+    document.querySelector('add__btn').addEventListener('click', function() {
+
+        
+    })
+
+})(budgetController, UIController);
